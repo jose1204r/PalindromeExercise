038223ee08d7739e80deefe9daf068ddba0e52bd
jose <jrodriguez@itsolutionspcllc.com>
2022-11-30 15:38:06 -0500

    PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WorldSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public  class WorldSmith
+    {
+
+        public bool IsAPalindrome(string word) 
+        
+        {
+            var reversed = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            if (reversed == word)
+            {
+
+                return false;
+            }
+
+            else { return true; }
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
+using PalindromeExercise;
 using System;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Theory]
+        [InlineData("recal", true )]
+        [InlineData("hello",false )]
+        
+        public void Test1(string word, bool expected)
+        {
+            // Arrage
+            var test = new WorldSmith();
+            // act
+            var actual = test.IsAPalindrome(word);
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
